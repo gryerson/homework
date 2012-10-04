470cf8532114aeb1839138d445fff0d8cb6f4cd9
gryerson <guy.ryerson@gmail.com>
2012-10-04 19:13:40 -0400

    first commit

---
+++ 2012-10-06/exercise1.php
@@ -0,0 +1,11 @@
+<?php
+
+		 $name = "Guy Ryerson";
+		 $birthday = "01-01-1966";
+		 $favorite_color = "red";
+		 
+		 print "My name is $name.<br />";
+		 print "My birthday is $birthday.<br />";
+		 print "My favorite color is $favorite_color.<br />";
+		 
+?>
