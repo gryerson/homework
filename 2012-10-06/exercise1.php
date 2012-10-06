d0c63b89869cec62f742e31e39a2e07551128563
gryerson <guy.ryerson@gmail.com>
2012-10-06 10:29:01 -0400

    second commit

--- 2012-10-06/exercise1.php
+++ 2012-10-06/exercise1.php
@@ -1,11 +1,9 @@
 <?php
+	$name = "Guy Ryerson";
+	$birthday = "01-01-1966";
+	$favorite_color = "red";
 
-		 $name = "Guy Ryerson";
-		 $birthday = "01-01-1966";
-		 $favorite_color = "red";
-		 
-		 print "My name is $name.<br />";
-		 print "My birthday is $birthday.<br />";
-		 print "My favorite color is $favorite_color.<br />";
-		 
+	print "My name is " . $name . ".<br />";
+	print "My birthday is " . $birthday . ".<br />";
+	print "My favorite color is " . $favorite_color . ".<br />";
 ?>
--- 2012-10-06/exercise2.php
+++ 2012-10-06/exercise2.php
@@ -1,13 +1,11 @@
 <?php
-
-		 $DaysPerWeek = 7;
-		 $HoursPerDay = 24;
-		 $MinutesPerHour = 60;
-		 
-		 print "The number of minutes per week on Earth are " . $DaysPerWeek * $HoursPerDay * $MinutesPerHour . ".<br />";
-		 
-		 $HoursPerDay = 26; // Number of hours in a day in the Bilky Way Galaxy
-		 
-		 print "The number of minutes per week in the Bilky Way Galaxy are " . $DaysPerWeek * $HoursPerDay * $MinutesPerHour . ".<br />";
-		  
+	$DaysPerWeek = 7;
+	$HoursPerDay = 24;
+	$MinutesPerHour = 60;
+	
+	print "The number of minutes per week on Earth are " . $DaysPerWeek * $HoursPerDay * $MinutesPerHour . ".<br />";
+	
+	$HoursPerDay = 26; // Number of hours in a day in the Bilky Way Galaxy
+	
+	print "The number of minutes per week in the Bilky Way Galaxy are " . $DaysPerWeek * $HoursPerDay * $MinutesPerHour . ".<br />";
 ?>
